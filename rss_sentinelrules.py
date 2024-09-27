6dd34fa1c2fb784a22c654e6b5eaee1d816b8183
hitem <8977898+hitem@users.noreply.github.com>
2024-09-27 13:22:46 +0200

    Update to structure

--- rss_sentinelrules.py
+++ rss_sentinelrules.py
@@ -215,7 +215,7 @@ if all_entries:
         # Build description for this chunk
         description_text = "Updated rules this week:<br/>"
         for entry in chunk:
-            description_text += f"<b>Name:</b> {entry['name']}<br/>"
+            description_text += f"<b>Name:</b> {entry['name']} ({entry['version']})<br/>"
             description_text += f"<b>ID:</b> <a href='{entry['url']}'>{entry['id']}</a><br/><br/>"
         
         # Add the description to the item
